b812675902a346a4323d69f8bb4d9e7e8936a670
Roman <Roman@ROMANPC>
2023-03-23 23:05:04 +0300

    Добавьте файлы проекта.

---
+++ ExominerSaveEditor/Program.cs
@@ -0,0 +1,12 @@
+﻿using Exominer;
+using System.Runtime.Serialization.Formatters.Binary;
+
+BinaryFormatter formatter = new();
+
+string saveString = null;
+
+using (FileStream fs = new FileStream("GameState", FileMode.Open))
+{
+    saveString = (string)formatter.Deserialize(fs);
+}
+
